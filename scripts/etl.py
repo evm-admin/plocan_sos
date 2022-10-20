27fd0fcf24ba63c305144a0110c01eb1ac643002
Elena Sacramento <elena.sacramento@evm.net>
2022-10-20 11:53:39 +0100

    Añadiendo comentario de oxígeno disuelto

--- scripts/etl.py
+++ scripts/etl.py
@@ -5,63 +5,42 @@
 # 'ESTUDIO Y SOPORTE TÉCNICO PARA LA IMPLEMENTACIÓN
 # DEL ESTÁNDAR SWE EN PLATAFORMA DE SENSORES
 # GESTIONADOS POR PLOCAN'
-# 
+#
 # Se tienen dos ficheros 'MERGED_ctd.csv' y 'MERGED_WEATHER.csv' que se van a limpiar y preparar para el script 'xml_creation_result.py'. Esta limpieza va a consistir en eliminar las columnas que no hagan falta para el proyecto, transformar las medidas en las unidades que se desee y crear un tercer fichero de salida que se corresponda con las variables medida por el DO.
 
-# In[19]:
-
-
 import pandas as pd
-import numpy as np
 
+# Ficheros de salida
+ctd_output_name = "./csv_cleaned/ctd_cleaned.csv"
+do_output_name = "./csv_cleaned/do_cleaned.csv"
+weather_output_name = "./csv_cleaned/weather_cleaned.csv"
+wg_output_name = "./csv_cleaned/wg_cleaned.csv"
 
-# In[20]:
+ctd = pd.read_csv("MERGED_ctd.csv")
+weather = pd.read_csv("MERGED_WEATHER.csv")
+telemetry = pd.read_csv("MERGED_TELEMETRY.csv")
 
-
-#Ficheros de salida
-ctd_output_name = './csv_cleaned/ctd_cleaned.csv'
-do_output_name = './csv_cleaned/do_cleaned.csv'
-weather_output_name = './csv_cleaned/weather_cleaned.csv'
-wg_output_name = './csv_cleaned/wg_cleaned.csv'
-
-
-# In[21]:
-
-
-ctd = pd.read_csv('MERGED_ctd.csv')
-weather = pd.read_csv('MERGED_WEATHER.csv')
-telemetry = pd.read_csv('MERGED_TELEMETRY.csv')
-
-
-# ### Modificaciones generales
-
-# In[26]:
-
-
-#Modificaciones generales a los csv
+# Modificaciones generales a los csv
 def clean_csv(df):
-    '''
+    """
     input:
         df: dataframe a limpiar
     output:
         df: dataframe limpio
-    '''
-    #Eliminación de columnas innecesarias
-    #df = df.drop(columns = ['Unnamed: 10', 'Unnamed: 11', 'Vehicle'])
+    """
+    # Eliminación de columnas innecesarias
+    # df = df.drop(columns = ['Unnamed: 10', 'Unnamed: 11', 'Vehicle'])
 
-    #Conversión de formatos
-    df['TimeStamp'] = pd.to_datetime(df['TimeStamp'], infer_datetime_format=True)
+    # Conversión de formatos
+    df["TimeStamp"] = pd.to_datetime(df["TimeStamp"], infer_datetime_format=True)
 
-    #Eliminamos duplicados de fecha, si existen
-    df = df.drop_duplicates('TimeStamp')
+    # Eliminamos duplicados de fecha, si existen
+    df = df.drop_duplicates("TimeStamp")
 
-    #Ordenamos en orden descendiente de fecha
-    df = df.sort_values(by = "TimeStamp", ascending = False)
-    
+    # Ordenamos en orden descendiente de fecha
+    df = df.sort_values(by="TimeStamp", ascending=False)
+
     return df
-
-
-# In[27]:
 
 
 ctd = clean_csv(ctd)
@@ -70,47 +49,54 @@ telemetry = clean_csv(telemetry)
 
 
 # ### Generación de csv CTD
-
-# In[28]:
-
-
-ctd_output = ctd[['TimeStamp', 'Pressure', 'Temperature', 'Conductivity', 'Latitude(deg)', 'Longitude(deg)']]
+ctd_output = ctd[
+    [
+        "TimeStamp",
+        "Pressure",
+        "Temperature",
+        "Conductivity",
+        "Latitude(deg)",
+        "Longitude(deg)",
+    ]
+]
 
 
 # ### Generación de csv DO
+do_output = ctd[
+    ["TimeStamp", "Oxygen", "Salinity (PSU)", "Latitude(deg)", "Longitude(deg)"]
+]
 
-# In[29]:
-
-
-do_output = ctd[['TimeStamp', 'Oxygen', 'Salinity (PSU)', 'Latitude(deg)', 'Longitude(deg)']]
-do_output['DO'] = ctd['Temperature'] * ctd['Pressure'] + do_output['Oxygen'] / do_output['Salinity (PSU)']
-do_output = do_output[['TimeStamp', 'Oxygen', 'Salinity (PSU)', 'DO', 'Latitude(deg)', 'Longitude(deg)']]
+# Simulación del cálculo del oxígeno disuelto (no es real)
+do_output["DO"] = (
+    ctd["Temperature"] * ctd["Pressure"]
+    + do_output["Oxygen"] / do_output["Salinity (PSU)"]
+)
+do_output = do_output[
+    ["TimeStamp", "Oxygen", "Salinity (PSU)", "DO", "Latitude(deg)", "Longitude(deg)"]
+]
 
 
 # ### Generación de csv weather
-
-# In[30]:
-
-
-weather_output = weather[['TimeStamp', 'Temperature(degC)', 'Wind Speed(kt)', 'Wind Gust Speed(kt)', 'Wind Direction', 
-                          'Pressure(mb)', 'Latitude', 'Longitude']]
-
+weather_output = weather[
+    [
+        "TimeStamp",
+        "Temperature(degC)",
+        "Wind Speed(kt)",
+        "Wind Gust Speed(kt)",
+        "Wind Direction",
+        "Pressure(mb)",
+        "Latitude",
+        "Longitude",
+    ]
+]
 
 # ### Generación WG
-
-# In[31]:
-
-
-wg_output = telemetry[['TimeStamp', 'Distance Over Ground(m)', 'Lat (deg)', 'Lon (deg)']]
-
+wg_output = telemetry[
+    ["TimeStamp", "Distance Over Ground(m)", "Lat (deg)", "Lon (deg)"]
+]
 
 # ### Guardado de csv
-
-# In[32]:
-
-
-ctd_output.to_csv(ctd_output_name, index = False)
-do_output.to_csv(do_output_name, index = False)
-weather_output.to_csv(weather_output_name, index = False)
-wg_output.to_csv(wg_output_name, index = False)
-
+ctd_output.to_csv(ctd_output_name, index=False)
+do_output.to_csv(do_output_name, index=False)
+weather_output.to_csv(weather_output_name, index=False)
+wg_output.to_csv(wg_output_name, index=False)
